2fef21aacf0683fab95232be45625e1ef2fea8f2
Julian Haeger <julianhaeger@gmail.com>
2014-07-30 18:23:23 +0100

    records number of stairs ascended

--- activityCapture.js
+++ activityCapture.js
@@ -1,3 +1,87 @@
+function calculateStepsClimbed(climbDetails) {
+	var startFloor = climbDetails.start.floor;
+	var endFloor = climbDetails.end.floor;
+
+	var stairsByFloor = {
+		'12': {
+			steps: 24,
+			calories: 17
+		},
+		'11': {
+			steps: 24,
+			calories: 17
+		},
+		'10': {
+			steps: 24,
+			calories: 17
+		},
+		'9': {
+			steps: 24,
+			calories: 17
+		},
+		'8': {
+			steps: 24,
+			calories: 17
+		},
+		'7': {
+			steps: 24,
+			calories: 17
+		},
+		'6': {
+			steps: 24,
+			calories: 17
+		},
+		'5': {
+			steps: 24,
+			calories: 17
+		},
+		'4': {
+			steps: 24,
+			calories: 17
+		},
+		'3': {
+			steps: 24,
+			calories: 17
+		},
+		'2': {
+			steps: 24,
+			calories: 17
+		},
+		'1': {
+			steps: 26,
+			calories: 18
+		},
+		'0': {
+			steps: 22,
+			calories: 15
+		},
+		'-1': {
+			steps: 16,
+			calories: 11
+		},
+		'-2': {
+			steps: 16,
+			calories: 11
+		},
+		'-3': {
+			steps: 16,
+			calories: 11
+		}
+	};
+
+	if (startFloor >= endFloor) {
+		return 0;
+	}
+
+	var totalSteps = 0;
+	for (var floor = endFloor; floor != startFloor; --floor) {
+		//console.log('looping. floor = ' + floor);
+		totalSteps += stairsByFloor[floor + ''].steps;
+	}
+
+	return totalSteps;
+}
+
 module.exports = function (db) {
 	return {
 		recordStairClimb: function (user, climbDetails) {
@@ -17,6 +101,8 @@ module.exports = function (db) {
 			};
 
 			climbDetails.durationInSeconds = ((climbDetails.end.time - climbDetails.start.time) / 1000);
+			climbDetails.stairsAscended = calculateStepsClimbed(climbDetails);
+			console.log('stairsAscended = ' + climbDetails.stairsAscended);
 
 			var activityRecord = {
 				type: 'climb',
