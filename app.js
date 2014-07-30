66b3d04037c1f74aebe6d4df5761b5c15d401ede
Julian Haeger <julianhaeger@gmail.com>
2014-07-30 10:16:09 +0100

    local auth works

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var express = require('express'),
     fs = require('fs'),
     http = require('http'),
     exphbs = require('express3-handlebars'),
-    lessMiddleware = require('less-middleware');
+    passport = require('./authentication/configurePassport');
+lessMiddleware = require('less-middleware');
 
 
 /*
@@ -18,7 +19,7 @@ var app = express();
 /* 
  * App Configurations
  */
-app.configure(function() {
+app.configure(function () {
     app.set('port', process.env.PORT || 5000);
 
     app.set('views', __dirname + '/views');
@@ -39,38 +40,59 @@ app.configure(function() {
     app.use(express.static(path.join(__dirname, 'public')));
 
     app.use(express.favicon());
-    app.use(express.logger('dev')); 
+    app.use(express.logger('dev'));
     app.use(express.methodOverride());
+    app.use(express.bodyParser());
+    app.use(express.cookieParser());
+    app.use(express.session({
+        secret: 'keyboard cfssdfsfsdt'
+    }));
+    app.use(passport.initialize());
+    app.use(passport.session());
     app.use(app.router);
 });
 
-app.configure('development', function(){
+app.configure('development', function () {
     app.use(express.errorHandler());
 });
 
 /*
-* Route for Index
-*/
-app.get('/', function(req, res) {
+ * Route for Index
+ */
+app.get('/', function (req, res) {
     res.render('index');
 });
+
+if (process.env.OFFLINE_MODE.toLowerCase() === 'true') {
+    app.get('/testing/login', function (req, res) {
+        res.render('localAuthLoginForm');
+    });
+
+    app.post('/testing/login', passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/testing/login',
+        failureFlash: false
+    }));
+} else {
+
+}
 
 
 /*
  * Routes for Robots/404
  */
-app.get('/robots.txt', function(req, res) {
-    fs.readFile(__dirname + "/robots.txt", function(err, data) {
+app.get('/robots.txt', function (req, res) {
+    fs.readFile(__dirname + "/robots.txt", function (err, data) {
         res.header('Content-Type', 'text/plain');
         res.send(data);
     });
 });
 
-app.get('*', function(req, res) {
+app.get('*', function (req, res) {
     res.render('404');
 });
 
 
-http.createServer(app).listen(app.get('port'), function() {
+http.createServer(app).listen(app.get('port'), function () {
     console.log("Express server listening on port " + app.get('port'));
 });
---
+++ authentication/configurePassport.js
@@ -0,0 +1,49 @@
+var passport = require('passport');
+module.exports = passport;
+
+passport.serializeUser(function (user, done) {
+	console.log('in serializeUser. user = ' + JSON.stringify(user));
+	done(null, user);
+});
+
+passport.deserializeUser(function (obj, done) {
+	//attach context engine here?
+	console.log("deserializeUser: " + JSON.stringify(obj));
+	done(null, obj);
+});
+
+if (process.env.OFFLINE_MODE.toLowerCase() === 'true') {
+	console.log('Using offline authentication Strategy');
+	var LocalStratergy = require('passport-local').Strategy;
+
+	function verificationFunction(username, password, done) {
+		console.log('in localStratergy auth verify method with ' + arguments);
+		var user = {
+			displayName: username,
+			username: username
+		};
+		return done(null, user);
+	}
+	passport.use(new LocalStratergy(verificationFunction));
+} else {
+	console.log('configuring twitter authentication Strategy');
+	var TwitterStrategy = require('passport-twitter').Strategy;
+	var config = require(process.env.CONFIG_PATH || '/etc/stepup.json');
+
+	passport.use(new TwitterStrategy({
+			consumerKey: config.twitter.consumerKey,
+			consumerSecret: config.twitter.consumerSecret,
+			callbackURL: "http://stairs.taggedstuff.net/auth/twitter/callback"
+		},
+		function (token, tokenSecret, profile, done) {
+			console.log('in twitter auth verify function');
+			done(null, profile);
+			// User.findOrCreate(..., function (err, user) {
+			// 	if (err) {
+			// 		return done(err);
+			// 	}
+			// 	done(null, user);
+			// });
+		}
+	));
+}
